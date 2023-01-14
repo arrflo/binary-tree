bc7c51ff99cca634ace5236b12419b3e3d0825e0
romalynfloranda <romalynfloranda0203@gmail.com>
2023-01-14 15:40:19 +0800

    5 main functions for exercise were coded

---
+++ binary-tree-exer-1.py
@@ -0,0 +1,93 @@
+# 1. find_min(): finds minimum element in entire binary tree
+# 2. find_max(): finds maximum element in entire binary tree
+# 3. calculate_sum(): calculates sum of all elements
+# 4. post_order_traversal(): performs post order traversal of a binary tree
+# 5. pre_order_traversal(): perofrms pre order traversal of a binary tree
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+#for find min ()
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
+#for find max ()
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+#for sum
+    def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return self.data + left_sum + right_sum
+
+#post order traversal
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.post_order_traversal()
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+#pre order traversal
+    def pre_order_traversal(self):
+        elements = [self.data]
+        if self.left:
+            elements += self.left.pre_order_traversal()
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
