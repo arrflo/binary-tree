89a7682b775a695b62751adb170e91b3d7914840
romalynfloranda <romalynfloranda0203@gmail.com>
2023-01-14 16:01:51 +0800

    program is cmplete

--- binary-tree-exer-1.py
+++ binary-tree-exer-1.py
@@ -91,3 +91,25 @@ class BinarySearchTreeNode:
             elements += self.right.pre_order_traversal()
 
         return elements
+        
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers = [15,12,7,14,27,20,23,88 ]
+
+    numbers_tree = build_tree(numbers)
+    print("Input numbers:",numbers)
+    print("Min:",numbers_tree.find_min())
+    print("Max:",numbers_tree.find_max())
+    print("Sum:", numbers_tree.calculate_sum())
+    print("In order traversal:", numbers_tree.in_order_traversal())
+    print("Pre order traversal:", numbers_tree.pre_order_traversal())
+    print("Post order traversal:", numbers_tree.post_order_traversal())
